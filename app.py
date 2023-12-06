03df1a37a4812daef563fd2af511b3d3b5b265c3
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2023-12-07 08:45:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ app=Flask(__name__)
 UPLOAD_FOLDER = os.getcwd() + '\\uploads'  # 절대 파일 경로
 
 
+# "./uploads": 업로드한 파일들을 저장하는 폴더
+# "home.html": 홈페이지, 파일 업로드 가능
+# "upload_result.html": 파일 업로드 결과 표시
+# "get_file.html": 업로드한 파일 목록 표시, 파일명 클릭 가능
+# "open_file.html": 파일 이름이 {{data}}인 파일을 브라우저에 렌더링하는 것이 목적, 현재 파일의 이름만 표시 가능한 상태
+
+
 @app.route('/')
 def home():    # 메인 경로
     return render_template("home.html")
@@ -37,13 +44,6 @@ def get_files():
 def uploaded_file():
     f = request.args.get('data')
     return render_template('open_file.html', data=f)
-
-
-
-# "home.html": 홈페이지, 파일 업로드 가능
-# "upload_result.html": 파일 업로드 결과 표시
-# "get_file.html": 업로드한 파일 목록 표시, 파일명 클릭 가능
-# "open_file.html": 파일 이름이 {{data}}인 파일을 브라우저에 렌더링하는 것이 목적, 현재 파일의 이름만 표시 가능한 상태
 
 
 if __name__=='__main__':
